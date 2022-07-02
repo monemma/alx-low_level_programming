5c2606fe0b987adde046abb25067a8a1d2b4bc98
monemma <mondayemmanuel400@gmail.com>
2022-07-02 14:13:00 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* * main - print the string in the put function
-* * Description: using the main function
-* * Return: 0
-* */
+* main - print the string in the put function
+* Description: using the main function
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
