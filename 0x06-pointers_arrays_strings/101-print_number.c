a1de46020cd89988dfbcd693abf2794958b2d07a
monemma <106972742+monemma@users.noreply.github.com>
2022-07-15 15:06:37 -0700

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,14 +8,14 @@
 void print_number(int n)
 
 {
-	unsigned int num = n;
-	if (n < 0)
+unsigned int num = n;
+if (n < 0)
 {
-	_putchar('-');
-	num = -num;
+_putchar('-');
+num = -num;
 }
-	if ((num / 10) > 0)
-	print_number(num / 10);
-	_putchar((num % 10) + '0');
+if ((num / 10) > 0)
+print_number(num / 10);
+_putchar((num % 10) + '0');
 
 }
